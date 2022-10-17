de12942baa5b8f727b955a85cae5f4ce361779de
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-10-16 23:02:01 -0400

    add masthead

--- client/src/components/Masthead/index.tsx
+++ client/src/components/Masthead/index.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styles from "./Masthead.module.css";
+import SearchBar from "../UI/Searchbar";
+import Button from "@mui/material/Button";
 
 export default function Masthead() {
-  return <div>Masthead</div>;
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <h1 className={styles.catchphrase}>
+        The price fluctuation is a conspiracy. Time to track our puchases.
+      </h1>
+      <div className={styles.circle}>
+        <div className={styles.wave}></div>
+      </div>
+      {/* <div className={styles.liquid}></div> */}
+      <SearchBar />
+      <Button
+        sx={{ backgroundColor: "var(--green-brand)" }}
+        type="submit"
+        variant="contained"
+      >
+        Submit
+      </Button>
+    </form>
+  );
 }
--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -4,7 +4,11 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <h1>Simply Orange</h1>
+      {/* <h1>Simply Orange</h1> */}
+      <img
+        src="https://vtlogo.com/wp-content/uploads/2020/04/simply-orange-vector-logo.png"
+        alt="brandlogo"
+      />
     </div>
   );
 }
